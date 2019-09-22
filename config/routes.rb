bb12aab0dccd9c7141fe3c4944fde88821d66a2a
Pete <prs148@hotmail.com>
2019-09-22 18:31:26 +0000

    dashboard with styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+  resource :dashboard, only: [:show]
   resources :courses, only: [:index, :show] do
     resources :enrollments, only: :create
   end
   resources :lessons, only: [:show]
   namespace :instructor do 
       resources :lessons, only: [:update]
-      resources :sections, only: [:update]
-      resources :sections, only: [] do
+      resources :sections, only: [:update] do
         resources :lessons, only: [:new, :create]
       end
       resources :courses, only: [:new, :create, :show] do
